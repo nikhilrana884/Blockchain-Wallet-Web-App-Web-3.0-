9ab58748d93ffc9b00ed74ef77d2eb1f280260de
nikhilrana884 <nikhilrana884@gmail.com>
2022-01-20 21:09:45 +0530

    Temp

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
                     <NavBarItem key={item + index} title={item} />
                 ))}
-                
+                {/* No Coding today bcoz of exam tomorrow. */} 
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     Login
                 </li>
